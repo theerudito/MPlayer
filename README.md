57ea38ee2f23b4ca0540a78d8cf4fbecfebe8232
Jorge <erudito.tv@gmail.com>
2026-06-05 09:40:25 -0500

    v1

--- README.md
+++ README.md
@@ -6,6 +6,46 @@ enable: true
 ios:
 enable: truecls
 
-
 flutter pub get
 dart run icons_launcher:create
+
+en android/app/build.gradle
+
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+signingConfigs {
+create("release") {
+keyAlias = keystoreProperties["keyAlias"] as String
+keyPassword = keystoreProperties["keyPassword"] as String
+storeFile = file(keystoreProperties["storeFile"] as String)
+storePassword = keystoreProperties["storePassword"] as String
+}
+}
+
+buildTypes {
+release {
+signingConfig = signingConfigs.getByName("debug")
+}
+}
+
+buildTypes {
+release {
+signingConfig = signingConfigs.getByName("release")
+}
+}
+
+flutter clean
+flutter pub get
+flutter build apk --release
+
+flutter build appbundle --release
+
+create a file named
+key.properties
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,16 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -26,9 +35,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
